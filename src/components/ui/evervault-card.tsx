b5ae3b6b90e63b7e3679ac56f34f33e14d06185f
VANSH AGARWAL <vanshagarwal11@gmail.com>
2026-04-10 23:35:56 +0530

    styles: Hero headline gaps

--- src/components/ui/evervault-card.tsx
+++ src/components/ui/evervault-card.tsx
@@ -51,11 +51,11 @@ export const EvervaultCard = ({
             <span className="dark:text-white text-black z-20 text-[1.5rem] text-center font-mono">
                 Hi, Welcome to 
                 <h3 className="text-[3rem] mt-3 font-bitcount font-normal">Vansh's Techscape</h3>
-                <ul className="flex gap-5 items-center justify-between text-[1rem] font-bold mt-8 bg-gradient-to-r from-green-400 to-blue-400  bg-clip-text text-transparent">
+                <ul className="flex gap-4 items-center justify-between text-[1rem] font-bold mt-8 bg-gradient-to-r from-green-400 to-blue-400  bg-clip-text text-transparent">
                     <li>Software Developer</li>
-                    <li>|</li>
+                    <li>•</li>
                     <li>Data Scientist</li>
-                    <li>|</li>
+                    <li>•</li>
                     <li>Quant Developer</li>
                 </ul>
             </span>
